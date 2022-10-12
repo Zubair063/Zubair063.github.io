10b95cf865cd98aad4c2cbda870c10af420929c2
Md.Zubair <45866517+Zubair063@users.noreply.github.com>
2022-10-12 08:12:42 +0600

    Standard Submit

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const sections = document.querySelectorAll('.section');
 const sectBtns = document.querySelectorAll('.controlls');
 const sectBtn = document.querySelectorAll('.control');
 const allSections = document.querySelector('.main-content');
+const submit_btn = document.querySelector('.main-btn1');
 
 
 
@@ -38,3 +39,64 @@ function PageTransitions(){
 }
 
 PageTransitions();
+
+
+
+// function email_page(){
+//   submit_btn.addEventListener('click', (e) => {
+//   // e.preventDefault();
+//   const id = e.target.dataset.id;
+//   // console.log(msg);
+//   // var name=document.getElementById('name').value;
+//   // var user=document.getElementById('email').value;
+//   // var subject=document.getElementById('subject').vlaue;
+//   // var msg=document.getElementById('body').value;
+//    console.log('hi');
+
+// //   Email.send({
+// //     Host : "smtp.gmail.com",
+// //     Username : "zubairhossain773@gmail.com",
+// //     Password : "qhnfevsahjlipnpu",
+// //     To : 'zubairhossain773@gmail.com',
+// //     From : user,
+// //     Subject : subject,
+// //     Body : msg
+// // }).then(
+// //   message => alert(message)
+// // );
+// })
+
+// }
+
+// email_page();
+
+// function email_page(){
+//   submit_btn.addEventListener('click', (e) => {
+//   e.preventDefault()
+//   var name=document.getElementById('name').value;
+//   var user=document.getElementById('email').value;
+//   var subjt=document.getElementById('sub').value;
+//   var msg=document.getElementById('body').value;
+
+//   // console.log(name);
+//   // console.log(user);
+//   // console.log(subjt);
+//   // console.log(msg);
+  
+//     Email.send({
+//     Host : "smtp.elasticemail.com",
+//     Username : "zubairhossain773@gmail.com",
+//     Password : "DFF361788149896F4D4F7CC56B7FB0EAFB81",
+//     To : 'zubairhossain773@gmail.com',
+//     From : 'mzh706@gmail.com',
+//     Subject : subjt,
+//     Body : msg
+// }).then(
+//   message => alert(message)
+// );
+
+// })
+
+// }
+
+// email_page();
